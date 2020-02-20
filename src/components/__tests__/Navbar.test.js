a207c48ea920a7e3d36779b68f68a5c042c6a57b
davidshare <davidessienshare@gmail.com>
2020-02-20 13:13:03 +0100

    use react-testing-library

--- src/components/__tests__/Navbar.test.js
+++ src/components/__tests__/Navbar.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import Navbar from '../Navbar/Navbar.component';
+
+afterEach(cleanup);
 
 describe('Navbar component', () => {
   test('render the app component', () => {
-    const wrapper = shallow(<Navbar />);
-    expect(wrapper).toMatchSnapshot();
-    expect(wrapper.find('img')).toHaveLength(1);
+    const { asFragment } = render(<Navbar />);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
